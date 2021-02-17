b80a5013f073307fe2f565f8551e371b705b3329
Peiyuan Xu <peiyuanxu303@gmail.com>
2021-02-17 15:27:21 -0600

    added two more questions

--- model_app/frontend/src/const/faqData.js
+++ model_app/frontend/src/const/faqData.js
@@ -19,6 +19,14 @@ const faqData = [
         question: "How is this simulator better than other simulators such as the Johns Hopkins Coronavirus Resource Center simulator?",
         answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."
     },
+    {
+        question: "Which algorithms are most commonly used in the simualtor?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."
+    },
+    {
+        question: "What can be said about the efficiency of the simulator, particurly on large-scale datasets?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."
+    },
     { 
         question: "How accurate is this simulator?",
         answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."
