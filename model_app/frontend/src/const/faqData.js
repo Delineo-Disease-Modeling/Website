93f4dc234302b46bbcc2440fe9c0fc7f11f70bb4
Peiyuan Xu <peiyuanxu303@gmail.com>
2021-02-17 14:47:27 -0600

    "added three more questions"

--- model_app/frontend/src/const/faqData.js
+++ model_app/frontend/src/const/faqData.js
@@ -6,8 +6,17 @@ const faqData = [
     {
         question: "What does this simulator do?",
         answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."
-        
-        question: "Which languages are used to program the simulator",
+    },
+    { 
+        question: "Which programming languages are used to program the simulator?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."
+    },
+    {
+        question: "Can this simulator be used for future pandemics?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."
+    },
+    { 
+        question: "How is this simulator better than other simulators such as the Johns Hopkins Coronavirus Resource Center simulator?",
         answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."
     },
 ];
