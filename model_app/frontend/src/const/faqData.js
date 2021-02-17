07186a9a02dc6232d29103a271ebaccc50ffc0bb
GWCMelody <melody2011102@gmail.com>
2021-02-16 16:56:15 -0800

    move faq data to const

---
+++ model_app/frontend/src/const/faqData.js
@@ -0,0 +1,12 @@
+const faqData = [
+    {
+        question: "What is this simulator?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."
+    },
+    {
+        question: "What does this simulator do?",
+        answer: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."
+    },
+];
+
+export default faqData;
