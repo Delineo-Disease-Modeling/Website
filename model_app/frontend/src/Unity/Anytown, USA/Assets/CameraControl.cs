dc0c9dafdd5da99df81c85a31e78eba5a23629a9
ChrisBJHU <chrisbakhit@gmail.com>
2021-02-19 02:58:35 -0500

    Updated Map

--- model_app/frontend/src/Unity/Anytown, USA/Assets/CameraControl.cs
+++ model_app/frontend/src/Unity/Anytown, USA/Assets/CameraControl.cs
@@ -5,43 +5,55 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Camera camera;
+    public float scrollSpeed = 10;
     void Start()
     {
-        
+        camera = GetComponent<Camera>();
+        camera.orthographicSize = 5; // Size u want to start with
     }
 
-    public float speed = 5.0f;
-    public float xboundtop = 0f;
-    public float xboundbottom = 0f;
-    public float yboundtop = 0f;
-    public float yboundbottom = 0f;
+    public float speed = 100;
+    public Transform obj;
+    public int maxX,maxY,minX,minY;
 
-
-    void Update()
+    public void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        float ScrollWheelChange = Input.GetAxis("Mouse ScrollWheel");
+        if (ScrollWheelChange != 0)
         {
-            transform.position = new Vector3(speed * Time.deltaTime, 0, 0);
+            camera.orthographicSize -= ScrollWheelChange * scrollSpeed;
+            if (camera.orthographicSize < 1)
+                    camera.orthographicSize = 1;
+            if (camera.orthographicSize > 15)
+                camera.orthographicSize = 15;
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        //Move only if we actually pressed something
+        if ((h > 0 || v > 0) || (h < 0 || v < 0))
         {
-            transform.position = new Vector3(speed * Time.deltaTime, 0, 0);
+            Vector3 tempVect = new Vector3(h, v, 0);
+            tempVect = tempVect.normalized * speed * Time.deltaTime;
+
+            Vector3 newPos = obj.transform.position + tempVect;
+            if (newPos.x > maxX || newPos.x < minX) newPos.x -= tempVect.x;
+            if (newPos.y < minY || newPos.y > maxY) newPos.y -= tempVect.y;
+            checkBoundary(newPos);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.position = new Vector3(0, speed * Time.deltaTime, 0);
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.position = new Vector3(0, speed * Time.deltaTime, 0);
-        }
-        if (transform.position.x > xboundtop)
-            transform.position = new Vector3(xboundtop, transform.position.y, transform.position.z);
-        if (transform.position.x < xboundbottom)
-            transform.position = new Vector3(xboundbottom, transform.position.y, transform.position.z);
-        if (transform.position.y > yboundtop)
-            transform.position = new Vector3(transform.position.x, yboundtop, transform.position.z);
-        if (transform.position.y > yboundbottom)
-            transform.position = new Vector3(transform.position.x, yboundbottom, transform.position.z);
+    }
+
+    void checkBoundary(Vector3 newPos)
+    {
+        //Convert to camera view point
+        Vector3 camViewPoint = Camera.main.WorldToViewportPoint(newPos);
+
+        //Apply limit
+        camViewPoint.x = Mathf.Clamp(camViewPoint.x, 0.04f, 0.96f);
+        camViewPoint.y = Mathf.Clamp(camViewPoint.y, 0.07f, 0.93f);
+
+        //Convert to world point then apply result to the target object
+        obj.position = Camera.main.ViewportToWorldPoint(camViewPoint);
     }
 }
