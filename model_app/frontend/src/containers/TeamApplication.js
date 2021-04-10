44a9ef6830c0fcdc32b30f779a2bb72adb42d57d
GWCMelody <melody2011102@gmail.com>
2021-04-10 17:09:49 -0400

    update on team app styling

--- model_app/frontend/src/containers/TeamApplication.js
+++ model_app/frontend/src/containers/TeamApplication.js
@@ -7,7 +7,24 @@ import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
+import StyledButton from '../components/StyledButton';
+import Card from '@material-ui/core/Card';
 import { Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+		fontWeight: 'normal',
+		letterSpacing: '1.2px',
+    color: '#222629'
+	},
+  border: {
+		width: '150px',
+		margin: 'auto',
+		borderTop: '3px solid #66FCF1',
+		marginBottom: '50px'
+	},
+}));
 
 const TeamApplication = function TeamApplication() {
 
@@ -21,114 +38,116 @@ const TeamApplication = function TeamApplication() {
   const [second_interested_team, setSecond_interested_team] = useState("");
   const [third_interested_team, setThird_interested_team] = useState("");
   const [submitMsg, setSubmitMsg] = useState("")
+  const classes = useStyles();
+
   return (
-    <>
+    <Container style={{ padding: '30px 20%', backgroundColor: '#66FCF1', margin: '0' }}>
+      <Card style={{ justifyContent: 'center', padding: '30px 15%', backgroundColor: 'white' }}>
 
-      <Container fixed>
+        <Box py={2} bgcolor="white" style={{ color: "#222629" }} align="center" >
+        <Typography variant="h3" component="h3" className={classes.title}>
+						JOIN TEAM
+					</Typography>
+					<div className={classes.border}></div>
 
-        <Box py={2} bgcolor="white" style={{ color: "black" }} align="left" >
+          <form noValidate autoComplete="on">
+            <Grid container direction={"column"} spacing={3}>
+              <Grid item>
+                <Typography >
+                  First Name
+                </Typography>
+                <TextField required id="standard-required" variant="outlined" label="First Name" onChange={e => setFirst_name(e.target.value)} />
+              </Grid>
+              <Grid item>
+                <Typography >
+                  Last Name
+                </Typography>
+                <TextField required id="standard-basic" variant="outlined" label="Last Name" onChange={e => setLast_name(e.target.value)} />
+              </Grid>
 
+              <Grid item>
+                <Typography >
+                  Major
+                </Typography>
+                <TextField required id="standard-basic" variant="outlined" label="Major" onChange={e => setMajor(e.target.value)} />
+              </Grid>
+              <Grid item>
+                <Typography  >
+                  Graduation Year
+                </Typography>
+                <TextField required id="standard-basic" variant="outlined" label="Graduation Year" onChange={e => setGrad_year(e.target.value)} />
+              </Grid>
 
-          <Typography style = {{fontSize:"40px"}}>
-            Join the team
-        </Typography>
-
-          <Grid container direction={"column"} spacing={2}>
-            <Grid item>
-              <Typography >
-                First Name
-        </Typography>
-              <TextField required id="standard-basic" variant="outlined" label="First Name" onChange={e => setFirst_name(e.target.value)} />
-            </Grid>
-            <Grid item>
-              <Typography >
-                Last Name
-        </Typography>
-              <TextField required id="standard-basic" variant="outlined" label="Last Name" onChange={e => setLast_name(e.target.value)} />
-            </Grid>
-
-            <Grid item>
-              <Typography >
-                Major
-        </Typography>
-              <TextField required id="standard-basic" variant="outlined" label="Major" onChange={e => setMajor(e.target.value)} />
-            </Grid>
-            <Grid item>
-              <Typography  >
-                Graduation Year
-        </Typography>
-              <TextField required id="standard-basic" variant="outlined" label="Graduation Year" onChange={e => setGrad_year(e.target.value)} />
-            </Grid>
-
-            <Grid item>
-              <Typography>First Choice Team</Typography>
-              <Select
-                onChange={(e) => {
-                  setFirst_interested_team(e.target.value);
-                }}
-                style = {{minWidth:"230px"}}
-              >
-                <MenuItem value="Fullstack" style = {{backgroundColor:"white"}}>Fullstack</MenuItem>
-                <MenuItem value="Simulation" style = {{backgroundColor:"white"}}>Simulation</MenuItem>
-                <MenuItem value="Machine learning" style = {{backgroundColor:"white"}}>Machine learning</MenuItem>
-                <MenuItem value="Information Support" style = {{backgroundColor:"white"}}>Information Support</MenuItem>
-              </Select>
-
-
-            </Grid>
-
-            <Grid item>
-              <Typography>Second Choice Team</Typography>
-              <Select
-                onChange={(e) => {
-                  setSecond_interested_team(e.target.value);
-                }}
-                style = {{minWidth:"230px"}}
-              >
-                <MenuItem value="Fullstack" style = {{backgroundColor:"white"}}>Fullstack</MenuItem>
-                <MenuItem value="Simulation" style = {{backgroundColor:"white"}}>Simulation</MenuItem>
-                <MenuItem value="Machine learning" style = {{backgroundColor:"white"}}>Machine learning</MenuItem>
-                <MenuItem value="Information Support" style = {{backgroundColor:"white"}}>Information Support</MenuItem>
-                <MenuItem value="None" style = {{backgroundColor:"white"}}>None</MenuItem>
-              </Select>
+              <Grid item>
+                <Typography>First Choice Team</Typography>
+                <Select
+                  onChange={(e) => {
+                    setFirst_interested_team(e.target.value);
+                  }}
+                  style={{ minWidth: "230px" }}
+                >
+                  <MenuItem value="Fullstack" style={{ backgroundColor: "white" }}>Fullstack</MenuItem>
+                  <MenuItem value="Simulation" style={{ backgroundColor: "white" }}>Simulation</MenuItem>
+                  <MenuItem value="Machine learning" style={{ backgroundColor: "white" }}>Machine learning</MenuItem>
+                  <MenuItem value="Information Support" style={{ backgroundColor: "white" }}>Information Support</MenuItem>
+                </Select>
 
 
-            </Grid>
+              </Grid>
 
-            <Grid item>
-              <Typography>Third Choice Team</Typography>
-              <Select
-                onChange={(e) => {
-                  setThird_interested_team(e.target.value);
-                }}
-                style = {{minWidth:"230px"}}
-              >
-                <MenuItem value="Fullstack" style = {{backgroundColor:"white"}}>Fullstack</MenuItem>
-                <MenuItem value="Simulation" style = {{backgroundColor:"white"}}>Simulation</MenuItem>
-                <MenuItem value="Machine learning" style = {{backgroundColor:"white"}}>Machine learning</MenuItem>
-                <MenuItem value="Information Support" style = {{backgroundColor:"white"}}>Information Support</MenuItem>
-                <MenuItem value="None"style = {{backgroundColor:"white"}}>None</MenuItem>
-              </Select>
+              <Grid item>
+                <Typography>Second Choice Team</Typography>
+                <Select
+                  onChange={(e) => {
+                    setSecond_interested_team(e.target.value);
+                  }}
+                  style={{ minWidth: "230px" }}
+                >
+                  <MenuItem value="Fullstack" style={{ backgroundColor: "white" }}>Fullstack</MenuItem>
+                  <MenuItem value="Simulation" style={{ backgroundColor: "white" }}>Simulation</MenuItem>
+                  <MenuItem value="Machine learning" style={{ backgroundColor: "white" }}>Machine learning</MenuItem>
+                  <MenuItem value="Information Support" style={{ backgroundColor: "white" }}>Information Support</MenuItem>
+                  <MenuItem value="None" style={{ backgroundColor: "white" }}>None</MenuItem>
+                </Select>
 
 
-            </Grid>
+              </Grid>
 
-            <Grid>
-              <Button >
-                Submit
-              </Button>
-            </Grid>
+              <Grid item>
+                <Typography>Third Choice Team</Typography>
+                <Select
+                  onChange={(e) => {
+                    setThird_interested_team(e.target.value);
+                  }}
+                  style={{ minWidth: "230px" }}
+                >
+                  <MenuItem value="Fullstack" style={{ backgroundColor: "white" }}>Fullstack</MenuItem>
+                  <MenuItem value="Simulation" style={{ backgroundColor: "white" }}>Simulation</MenuItem>
+                  <MenuItem value="Machine learning" style={{ backgroundColor: "white" }}>Machine learning</MenuItem>
+                  <MenuItem value="Information Support" style={{ backgroundColor: "white" }}>Information Support</MenuItem>
+                  <MenuItem value="None" style={{ backgroundColor: "white" }}>None</MenuItem>
+                </Select>
 
-            <Grid>
-              <Typography style={{ color: "red" }}>
-                {submitMsg}
-              </Typography>
-            </Grid>
 
-          </Grid >
+              </Grid>
+
+              <Grid>
+                <StyledButton style={{ color: '#222629', marginTop: '30px' }}>
+                  Submit
+              </StyledButton>
+              </Grid>
+
+              <Grid>
+                <Typography style={{ color: "red" }}>
+                  {submitMsg}
+                </Typography>
+              </Grid>
+
+            </Grid >
+          </form>
         </Box>
-      </Container>
-    </>
+      </Card>
+    </Container>
   )
 }
 export default TeamApplication;
