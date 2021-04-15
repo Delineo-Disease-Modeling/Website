d63cabc82d04794474921e48f1556efb36c8d727
GWCMelody <melody2011102@gmail.com>
2021-04-15 17:32:05 -0400

    added some edits that got lost

--- model_app/frontend/src/containers/About.js
+++ model_app/frontend/src/containers/About.js
@@ -109,7 +109,30 @@ const styles = {
 	faqSlots: {
 		textAlign: 'left',
 		padding: '0 50px'
-	}
+	},
+	input: {
+		padding: '10px',
+		minWidth: '300px',
+		margin: '10px 30px'
+	},
+	form: {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center'
+	},
+	button: {
+		marginTop: '30px'
+	},
+	contactSection: {
+		paddingTop: '70px'
+	},
+	description: {
+		fontSize: '16px',
+		letterSpacing: '1.2px',
+		color: 'black',
+		paddingBottom: '50px',
+		paddingTop: '50px'
+	},
 }
 
 const ColoredAccordion = withStyles({
@@ -142,7 +165,6 @@ const ColAccordion = withStyles({
 		},
 		'&$expanded': {
 			margin: 'auto',
-			boxShadow: 'inset 0px 0px 5px black',
 		},
 	},
 	expanded: {},
@@ -154,7 +176,6 @@ const ColAccordionSummary = withStyles({
 		borderBottom: '0px solid rgba(0, 0, 0, .125)',
 		marginBottom: 0,
 		minHeight: 56,
-		borderRadius: '15px',
 		'&$expanded': {
 			minHeight: 56,
 		},
@@ -210,12 +231,12 @@ class About extends React.Component {
 											expandIcon={<ExpandMoreIcon />}
 											aria-controls="Model Param-content"
 											id="Model Param-header"
-											style={{ padding: '0 50px' }}
+											style={{ padding: '0 50px', fontWeight: '600' }}
 										>
 											{faq.question}
 										</ColAccordionSummary>
 										<AccordionDetails style={{ padding: '0 50px' }}>
-											<Typography style={{ color: 'black', paddingBottom: '50px' }}>
+											<Typography className={classes.description}>
 												{faq.answer}
 											</Typography>
 										</AccordionDetails>
