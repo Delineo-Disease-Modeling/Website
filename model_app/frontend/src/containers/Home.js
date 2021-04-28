c4be2873204b0481afe205ea2f6684b9190d1dbf
Simone <70237983+s-bliss@users.noreply.github.com>
2021-04-27 22:08:37 -0400

    Revert "pr"

--- model_app/frontend/src/containers/Home.js
+++ model_app/frontend/src/containers/Home.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core';
+import BlogCard from '../components/BlogCard'
+import blogfake1 from '../images/blogfake1.jpg';
+import blogfake2 from '../images/blogfake2.jpg';
+import blogfake3 from '../images/blogfake3.jpg';
 import HomeCarousel from '../components/Carousel';
 import TeamMemberJoin from '../images/teammemberjoin.jpg'
-import blogbackground from '../images/blogbackgroud.PNG'
-import Link from '@material-ui/core/Link';
-import DevelopmentBlogCards from './DevelopmentBlogCards.js'
-import anytownScreenshot from '../images/anytownScreenshot.png'
 
 const styles = {
+    button: {
+        borderColor: '#66FCF1',
+        backgroundColor: '#66FCF1',
+        border: '1px solid',
+        color: '#444f56',
+        "&:hover": {
+            backgroundColor: '#222629',
+            color: '#66FCF1',
+            borderColor: '#66FCF1',
+        },
+    },
 
     bigButton: {
         borderColor: '#66FCF1',
@@ -25,9 +37,14 @@ const styles = {
         fontSize: '20px'
     },
 
+    buttonContainer: {
+        width: '100%'
+    },
+
 
     imageBox: {
         background: "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5))",
+        backgroundImage: "url('https://9to5mac.com/wp-content/uploads/sites/6/2020/01/Coronavirus.jpg?quality=82&strip=all')",
         backgroundSize: "cover",
 
         color: "white",
@@ -43,38 +60,86 @@ const styles = {
         background: "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5))",
         backgroundSize: "cover",
         color: "white",
-        height: "130vh",
+        height: "100vh",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        opacity: "0.7",
+        opacity: "0.9",
         boxShadow: "inset 0 0 0 100vw rgba(0,0,0,0.5)",
     },
 
-    blogSection: {
-        height: "80vh",
-        padding: '20px 200px 60px 200px',
+    aboutSection: {
+        padding: '100px 110px 100px 110px',
+        backgroundColor: '#222D3C', /* Or #1C2531 ? */
         overflow: 'hidden',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        
 
+        " p": {
+            fontSize: '18px',
+        }
+    },
+
+    whiteBackground: {
+        backgroundColor: 'white',
+        marginBottom: '50px',
+        borderRadius: '30px 30px 30px 30px'
+    },
+
+    blogSection: {
+
+        padding: '20px 200px 60px 200px',
+        backgroundColor: '#d5d7db',
+        overflow: 'hidden',
+
+        "> p": {
+            fontSize: '18px',
+            marginTop: '1rem',
+            marginBottom: '2rem',
+            color: 'lightgrey'
+        }
     },
 
     recent: {
-        color: 'black',
+        color: '#212628',
         //fontStyle: 'strong',
         fontSize: "40px",
-        marginTop: "30px",
-        marginBottom:"50px"
-        
+        marginTop: "30px"
     },
 
-    link: {
-        '&:hover': {
-            color: 'white',
-        }
-      }
+    blogImg: {
+        backgroundColor: 'white',
+        maxWidth: ' 100%',
+        maxHeight: '100%',
+        borderRadius: '30px 30px 30px 30px',
+        overflow: 'hidden',
+    },
+
+    blogContainer: {
+        paddingTop: '30px',
+        textAlign: 'center',
+        backgroundColor: '#212628',
+        borderRadius: '30px 30px 30px 30px',
+        minHeight: '600px',
+        overflow: 'hidden',
+        margin: '25px'
+    },
+
+    blogText: {
+        fontSize: '18px',
+        marginTop: '1rem',
+        marginBottom: '2rem',
+        color: '#d8e2ee',
+        textAlign: 'left',
+    },
+
+    blogContent: {
+        padding: '20px'
+    },
+
+    imgContainer: {
+        padding: '0px 20px 20px 20px'
+    },
+
+
 };
 
 
@@ -88,41 +153,58 @@ class Home extends Component {
         return (
             <React.Fragment>
                 <CssBaseline />
-                <div className={classes.imageBox} style={{ backgroundImage:`url(${anytownScreenshot})`}}>
+                <div className={classes.imageBox}>
                     <div className="header-text">
                         <h1 style={{ fontSize: '50px', marginBottom: "30px"}}>Interactive Disease Simulation</h1>
-                        <h5 style={{marginBottom: "30px"}}>Bring a fresh approach to the challenge of modeling the spread of pandemics</h5>
+                        <h5 style={{marginBottom: "30px"}}>Bringing a fresh approach to modeling the spread of pandemics</h5>
                         <Button className={classes.bigButton} variant="contained" color="inherit" href="/simulator">
-                        Visit Anytown, USA 
+                            Explore Simulation
                         </Button>
                     </div>
                 </div>
 
                 <HomeCarousel/>
 
-                <div className={classes.blogSection} style={{ backgroundImage:`url(${blogbackground})`}}>
-                    <Typography className={classes.recent}>
-                    Check Out Our Blog
+                <div className={classes.blogSection}>
+                    <Typography variant="h4" component="h2" gutterBottom className={classes.recent}>
+                        Read Our Blog Posts
                     </Typography>
 
-                    <DevelopmentBlogCards/>
+                    <Grid container spacing={10} >
+                        <Grid item xs={4}>
+                            <BlogCard
+                            imgURL = {blogfake1}
+                            title = {"Ex-CDC head claims without evidence Covid-19 began in a China lab"}
+                            brief = {"The crisis in Papua New Guinea is a reminder that the virus will keep wreaking havoc until the whole world is vaccinated."}/>
+                            
+
+                        </Grid>
+                        <Grid item xs={4}>
+                            <BlogCard                            
+                            imgURL = {blogfake2}
+                            title = {"This Island Nation Had Zero Covid Cases for Months"}
+                            brief = {"The crisis in Papua New Guinea is a reminder that the virus will keep wreaking havoc until the whole world is vaccinated."}/>
+                            
+                        </Grid>
+                        <Grid item xs={4}>
+                            <BlogCard                            
+                            imgURL = {blogfake3}
+                            title = {"White House reviews whether to lift intellectual property shield on Covid-19 vaccines"}
+                            brief = {"The crisis in Papua New Guinea is a reminder that the virus will keep wreaking havoc until the whole world is vaccinated."}/>
+
+                        </Grid>
+                    </Grid>
                 </div>
                 <div className={classes.teamPage} style={{ backgroundImage:`url(${TeamMemberJoin})`}}>
                     <div className="header-text">
                         <h1 style={{ fontSize: '50px', marginBottom: "30px"}}>We Are Delineo</h1>
                         <h5 style={{marginBottom: "30px"}}>Bring a fresh approach to the challenge of modeling the spread of pandemics</h5>
-                        <Button className={classes.bigButton} variant="contained" color="inherit" href="/team">
-                            Meet the Team
+                        <Button className={classes.bigButton} style={{marginRight: "30px"}} variant="contained" color="inherit" href="/simulator">
+                            Meet Our Team
                         </Button>
-                        <h5 style={{marginTop: "30px"}}>
-                        <Link  className={classes.link} color="inherit" href="/teamapplication">
-                            Interested in joining our team?
-                        </Link >
-                        </h5>
-
-                    </div>
-                    <div>
-
+                        <Button className={classes.bigButton} style={{marginLeft: "30px"}} variant="contained" color="inherit" href="/simulator">
+                            Join Our Team
+                        </Button>
                     </div>
                 </div>
 
