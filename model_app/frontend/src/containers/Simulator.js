8a8fa94dfc9f508a717258fb9d9311202798d1f6
Ryanqaz123 <43908954+Ryanqaz123@users.noreply.github.com>
2021-06-03 01:59:55 -0400

    update

--- model_app/frontend/src/containers/Simulator.js
+++ model_app/frontend/src/containers/Simulator.js
@@ -10,10 +10,10 @@ import Unity, { UnityContext } from "react-unity-webgl";
 import Typography from "@material-ui/core/Typography";
 
 const unityContext = new UnityContext({
-  loaderUrl: "./Build/JuneBuild.loader.js",
-  dataUrl: "./Build/JuneBuild.data",
-  frameworkUrl: "./Build/JuneBuild.framework.js",
-  codeUrl: "./Build/JuneBuild.wasm",
+  loaderUrl: "./Build/May21.loader.js",
+  dataUrl: "./Build/May21.data",
+  frameworkUrl: "./Build/May21.framework.js",
+  codeUrl: "./Build/May21.wasm",
 });
 
 /*
