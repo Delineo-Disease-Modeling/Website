037c8452ca9ad2163c19904ef17ae836dcd2f135
GWCMelody <melody2011102@gmail.com>
2021-06-18 10:12:01 -0700

    fixed stretched pictures (part b)

--- model_app/frontend/src/components/CarouselSection.js
+++ model_app/frontend/src/components/CarouselSection.js
@@ -5,21 +5,23 @@ import Typography from '@material-ui/core/Typography'
 
 export default function CarouselSection(captions, images) {
     return (
-        <Carousel>
-            {
-                [0, 1, 2, 3].map(element => {
-                    return(<Item key={element} caption={captions[element]} image={images[element]} />)
-                })
-            }
-        </Carousel>
+        <Paper style={{ padding: '30px', borderRadius: '15px', backgroundColor: 'transparent', border: '2px solid black', width: '100%' }}>
+            <Carousel>
+                {
+                    [0, 1, 2, 3].map(element => {
+                        return (<Item key={element} caption={captions[element]} image={images[element]} />)
+                    })
+                }
+            </Carousel>
+        </Paper>
     )
 }
 
 function Item(props) {
     return (
-        <Paper style={{padding: '30px', borderRadius: '15px', backgroundColor: 'transparent', border: '2px solid black'}} >
-            <Typography varient='h2' component='h2' style={{color: 'black', fontSize: '36px'}}>{props.caption}</Typography>
-            <img alt="" style={{width: '100%', height: '400px'}} src={props.image} />
-        </Paper>
+        <div style={{backgroundColor: 'transparent'}}>
+            <Typography varient='h2' component='h2' style={{ color: 'black', fontSize: '36px' }}>{props.caption}</Typography>
+            <img alt="" style={{ width: 'auto', height: '70vh' }} src={props.image} />
+        </div>
     )
 }
