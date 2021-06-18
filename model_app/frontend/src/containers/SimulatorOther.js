9ec7baca549c641b7efc6b029b03d09250acf105
Jonathan Bakhit <Jonathanbakhit02@gmail.com>
2021-06-17 20:47:15 -0500

    Made Sliders and switches

--- model_app/frontend/src/containers/SimulatorOther.js
+++ model_app/frontend/src/containers/SimulatorOther.js
@@ -1,9 +1,5 @@
 import React from "react";
 import { withStyles } from '@material-ui/core/styles';
-import MuiAccordion from '@material-ui/core/Accordion';
-import MuiAccordionSummary from '@material-ui/core/AccordionSummary';
-import MuiAccordionDetails from '@material-ui/core/AccordionDetails';
-import './SimulatorOther.css'
 import Typography from "@material-ui/core/Typography";
 import TopRight from "../images/temp/TopRightGraph.PNG";
 import TopLeft from "../images/temp/TopGraph.PNG";
@@ -13,62 +9,21 @@ import BottomRight from "../images/temp/BottomRightImage.PNG";
 import MiddleImage from "../images/temp/MiddleImage.PNG";
 import { styled } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Slider from '@material-ui/core/Slider';
+import Switch from '@material-ui/core/Switch';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+
 const styles = {
 	cardBackground: {
 		backgroundColor: '#2f3538',
 		alignContent: 'center',
 		paddingBottom: '1%',
 	},
-	underline: {
-		width: '200px',
-		margin: 'auto',
-		borderTop: '3px solid #66FCF1',
-		marginBottom: '30px'
-	},
-	title: {
-		position: 'absolute',
-		top: '190px',
-		left: '20%',
-		letterSpacing: '1.5px',
-		lineHeight: '50px',
-		textShadow: '2px 2px 15px black',
-		textAlign: 'left'
-	},
-	subtitle: {
-		position: 'absolute',
-		top: '320px',
-		left: '20%',
-		fontSize: '18px',
-		textShadow: '2px 2px 10px black',
-		textAlign: 'left'
-	},
-	border: {
-		position: 'absolute',
-		top: '300px',
-		left: '20%',
-		width: '100px',
-		margin: 'auto',
-		borderTop: '3px solid #66FCF1',
-		marginBottom: '50px'
-	},
-	faq: {
-		background: '#222629',
-		padding: '100px 0',
-	},
-	faqSlots: {
-		textAlign: 'left',
-		padding: '0 50px'
-	},
-	input: {
-		padding: '10px',
-		minWidth: '300px',
-		margin: '10px 30px'
-	},
-	form: {
-		display: 'flex',
-		flexDirection: 'column',
-		alignItems: 'center'
-	},
+	
+	
+	
 	button: {
 		marginTop: '30px'
 	},
@@ -84,25 +39,14 @@ const styles = {
 		textAlign: "center",
 		alignSelf: "center",
 		alignItems: "center",
-		margin: "100px 200px",
+		margin: "100px 315px",
 		borderRadius: "25px",
 		border: "2px solid #2f3538",
 		backgroundColor: "#c4c4c4",
 	  },
 	
-	  flexContainer: {
-		padding: 0,
-	
-		backgroundColor: "#2f3538",
-		color: "#2f3538",
-	
-		margin: "0 auto",
-	  },
-	
-	  backgroundColor: {
-		backgroundColor: "#2f3538",
-		color: "#2f3538",
-	  },
+	  
+  
 	
 	  boldTitle: {
 		top: "300px",
@@ -111,29 +55,13 @@ const styles = {
 		fontSize: 50,
 		},
 	
-	  h2: {
-		color: "white",
-		marginBottom: 10,
-		fontSize: 30,
-	  },
-	
-	  h3: {
-		color: "white",
-		marginRight: 100,
-		marginLeft: 100,
-		width: "fit-content",
-	
-		fontSize: 15,
-		backgroundColor: "#2f3538",
-		maxWidth: "1000px",
-	  },
 	
 	
 	  topdataleft: {
 		width:"300x",
 		height:"200px",
 		position: "absolute",
-		left: "50px",
+		left: "200px",
 		borderRadius: "25px",
 		border: "2px solid #2f3538",
 		top: "115px",
@@ -144,7 +72,7 @@ const styles = {
 		width:"400x",
 		height:"135px",
 		position: "absolute",
-		left: "50px",
+		left: "200px",
 		top: "345px", 
 		borderRadius: "25px",
 		border: "2px solid #2f3538",
@@ -158,7 +86,7 @@ const styles = {
 		width:"300x",
 		height:"200px",
 		position: "absolute",
-		right: "50px",
+		right: "200px",
 		top: "115px", 
 			borderRadius: "25px",
 		border: "2px solid #2f3538",
@@ -166,13 +94,84 @@ const styles = {
 	  },
 	
 	  bottomdataright: {
-		width:"130x",
+		width:"300x",
 		height:"108px",
 		position: "absolute",
-		right: "50px",
+		right: "195px",
 		top: "345px", 
 	
 	  },
+	  sliderMask_Wearing: {
+		width: '210px',
+		height:"20px",
+		position: "absolute",
+		top: 585,
+		left: 370,
+
+	  },
+	  sliderCap_Restrictions: {
+		width: '210px',
+		height:"20px",
+		position: "absolute",
+		top: 627.5,
+		left: 370,
+
+	  },
+	  sliderMass_Testing: {
+		width: '210px',
+		height:"20px",
+		position: "absolute",
+		top: 670,
+		left: 370,
+
+	  },
+	  sliderVacc_Pop: {
+		width: '210px',
+		height:"20px",
+		position: "absolute",
+		top: 585,
+		right:450,
+
+	  },
+
+	  StayAtHome: {
+		width: '300px',
+		height:"10px",
+		position: "absolute",
+		top: 200,
+		right: 270,
+	  },
+	  Schools: {
+		width: '210px',
+		height:"10px",
+		position: "absolute",
+		top: 80,
+		right: -50,
+	  },
+	  Restaurants: {
+		width: '210px',
+		height:"10px",
+		position: "absolute",
+		top: 120,
+		right: -50,
+	  },
+	  Gyms: {
+		width: '210px',
+		height:"10px",
+		position: "absolute",
+		top: 160,
+		right: -50,
+	  },
+	  Bars: {
+		width: '210px',
+		height:"10px",
+		position: "absolute",
+		top: 200,
+		right: -50,
+	  },
+	  blueSwitch: {
+		color: "#primary",
+	  }
 	};
 	
 	const StyledButton = styled(Button)({
@@ -190,52 +189,59 @@ const styles = {
 		  boxShadow: '0px 0px 10px #222629',
 	  },
 	});
+	const NewButton = styled(Button)({
+		position: "absolute",
+		right: '465px',
+		top: '700px',
+		bottom: '25px',
+		height: 45,
+		padding: '5px 60px 5px',
+		borderRadius: 3,
+		borderColor: '#2f3538',
+		backgroundColor: '#2f3538',
+		border: '1px solid',
+		color: '#ffffff',
+		"&:hover": {
+			backgroundColor: '#2f3538',
+			color: '#ffffff',
+			borderColor: '#2f3538',
+			boxShadow: '0px 0px 10px #222629',
+		},
+	  });
 
-const AccordionDetails = withStyles((theme) => ({
-	root: {
-		padding: theme.spacing(2),
-	},
-}))(MuiAccordionDetails);
+	  const SliderTemplate = withStyles({
+		root: {
+			color: '#62666e',
+			height: 8,
+		  },
+		  thumb: {
+			height: 18,
+			width: 18,
+			backgroundColor: '#b8fbfc',
+			border: '2px solid currentColor',
+			marginTop: -6,
+			marginLeft: -8,
+			'&:focus, &:hover, &$active': {
+			  boxShadow: 'inherit',
+			},
+		  },
+		  active: {},
+		  valueLabel: {
+			left: 'calc(-50% + 4px)',
+		  },
+		  track: {
+			height: 6,
+			borderRadius: 4,
+		  },
+		  rail: {
+			height: 6,
+			borderRadius: 4,
+		  },
+	  })(Slider);
+	  
 
-const ColAccordion = withStyles({
-	root: {
-		backgroundColor: 'white',
-		fontSize: '20px',
-		color: 'black',
-		border: '0px solid rgba(0, 0, 0, .125)',
-		boxShadow: '5px 5px 5px black',
-		borderRadius: '20px',
-		'&:not(:last-child)': {
-			borderBottom: 0,
-		},
-		'&:before': {
-			display: 'none',
-		},
-		'&$expanded': {
-			margin: 'auto',
-		},
-	},
-	expanded: {},
-})(MuiAccordion);
 
-const ColAccordionSummary = withStyles({
-	root: {
-		backgroundColor: '#66FCF1',
-		borderBottom: '0px solid rgba(0, 0, 0, .125)',
-		marginBottom: 0,
-		minHeight: 56,
-		'&$expanded': {
-			minHeight: 56,
-		},
-	},
-	content: {
-		'&$expanded': {
-			margin: '12px 0',
-		},
-	},
-	expanded: {
-	},
-})(MuiAccordionSummary);
+
 
 class SimulatorOther extends React.Component {
 
@@ -272,13 +278,58 @@ class SimulatorOther extends React.Component {
     </div>
     <div className={classes.bubble} align="center">
       <div>
-      <img src={BottomSection} style = {styles.bottom} alt = "Bottom Panel"/>
+	  <SliderTemplate style = {styles.sliderMask_Wearing} valueLabelDisplay = "auto" defaultValue ={0} width = '50px'/>
+	  <SliderTemplate style = {styles.sliderCap_Restrictions} valueLabelDisplay = "auto" defaultValue ={0} width = '50px'/>
+	  <SliderTemplate style = {styles.sliderMass_Testing} valueLabelDisplay = "auto" defaultValue ={0} width = '50px'/>
+	  <NewButton>Confirm</NewButton>
+      
+	  <FormControl component="fieldset" >
+      <FormGroup aria-label="position" row>
+        <FormControlLabel 
+		  style = {styles.StayAtHome}
+          value="left"
+          control={<Switch style = {styles.blueSwitch} />}
+          label="Stay-At-Home Order"
+          labelPlacement="Start"
+        />
+		<FormControlLabel 
+		  style = {styles.Schools}
+          value="left"
+          control={<Switch style = {styles.blueSwitch} />}
+          label="Schools"
+          labelPlacement="Start"
+        />
+		<FormControlLabel 
+		  style = {styles.Restaurants}
+          value="left"
+          control={<Switch style = {styles.blueSwitch} />}
+          label="Restaurants"
+          labelPlacement="Start"
+        />
+		<FormControlLabel 
+		  style = {styles.Gyms}
+          value="left"
+          control={<Switch style = {styles.blueSwitch}/>}
+          label="Gyms"
+          labelPlacement="Start"
+        />
+		<FormControlLabel 
+		  style = {styles.Bars}
+          value="left"
+          control={<Switch style = {styles.blueSwitch} />}
+          label="Bars"
+          labelPlacement="Start"
+        />
+		</FormGroup>
+	</FormControl>
+	<SliderTemplate style = {styles.sliderVacc_Pop} valueLabelDisplay = "auto" defaultValue ={0} width = '50px'/>
+	
+
       </div>
-      <StyledButton>Confirm</StyledButton>
       </div>
       </div>
       </div>
-			</div >
+	</div >
 		);
 	}
 }
