f3614768f861db0d57257084466a80aa7622a0fc
SP-Zeng <77908144+SP-Zeng@users.noreply.github.com>
2021-06-19 21:18:26 +0800

    update simulation background pic

--- model_app/frontend/src/components/Carousel.js
+++ model_app/frontend/src/components/Carousel.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import Carousel from 'react-bootstrap/Carousel'
 import { makeStyles } from '@material-ui/core/styles';
-import homepagecode from '../images/simulator_img.png'
+import homepagecode from '../images/simulator_background.jpg'
 import homepageml from '../images/ML_background.jpg'
 
 const useStyles = makeStyles({
