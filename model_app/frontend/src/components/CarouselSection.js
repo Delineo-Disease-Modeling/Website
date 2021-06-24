0bd4af6adf957ca43f67938cd606845042921d38
GWCMelody <melody2011102@gmail.com>
2021-06-24 11:39:15 -0700

    about page touch-up

--- model_app/frontend/src/components/CarouselSection.js
+++ model_app/frontend/src/components/CarouselSection.js
@@ -3,13 +3,13 @@ import Carousel from 'react-material-ui-carousel'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 
-export default function CarouselSection(captions, images) {
+export default function CarouselSection(captions, alts, images) {
     return (
-        <Paper style={{ padding: '0px', backgroundColor: 'white', border: '2px solid black', width: '90%', margin:'auto' }}>
+        <Paper style={{ padding: '15px', backgroundColor: 'transparent', border: '2px solid black', width: '90%', margin:'auto' }}>
             <Carousel>
                 {
                     [0, 1].map(element => {
-                        return (<Item key={element} caption={captions[element]} image={images[element]} />)
+                        return (<Item key={element} caption={captions[element]} image={images[element]} alt={alts[element]}/>)
                     })
                 }
             </Carousel>
@@ -20,8 +20,8 @@ export default function CarouselSection(captions, images) {
 function Item(props) {
     return (
         <div style={{ backgroundColor: 'transparent' }}>
-            <img alt="" style={{ width: '100%', height: 'auto' }} src={props.image} />
-            <Typography varient='h4' component='h4' style={{ marginTop: '10px', color: '#222629'}}>{props.caption}</Typography>
+            <img alt={props.alt} style={{ width: '100%', height: 'auto' }} src={props.image} />
+            <Typography varient='h4' component='h4' style={{ marginTop: '10px', color: 'white'}}>{props.caption}</Typography>
         </div>
     )
 }
