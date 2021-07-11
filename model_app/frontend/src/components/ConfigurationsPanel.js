42a1c8835794db0b565b659c6d59f9377d807e6c
Shanelle Cao <shanelle.cao@Shanelles-MacBook-Pro.local>
2021-07-11 02:59:24 -0700

    improved size and spacing & made responsive

--- model_app/frontend/src/components/ConfigurationsPanel.js
+++ model_app/frontend/src/components/ConfigurationsPanel.js
@@ -10,6 +10,7 @@ import Switch from '@material-ui/core/Switch';
 import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
 import InputAdornment from '@material-ui/core/InputAdornment';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -20,16 +21,33 @@ const useStyles = makeStyles((theme) => ({
         borderRadius: '20px',
         justifyContent: 'space-around'
     },
+    mobileRoot: {
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: 'rgba(196,196,196,255)',
+        margin: '2%',
+        paddingBottom: '1%',
+        borderRadius: '20px',
+        justifyContent: 'center',
+    },
     configurationsHeader: {
         backgroundColor: 'rgba(49,53,56,255)',
+        fontSize: "1.2rem",
         paddingTop: '1%',
         paddingBottom: '1%',
+        marginBottom: '2%',
         borderRadius: '5px'
     },
     panelSection: {
         textAlign: 'left',
         paddingTop: "2%",
-        width: "30%"
+        width: "30%",
+    },
+    mobilePanelSection: {
+        textAlign: 'left',
+        margin: 'auto',
+        paddingTop: "5%",
+        width: "90%",
     },
     confirmButtonPanel: {
         textAlign: 'left',
@@ -42,29 +60,52 @@ const useStyles = makeStyles((theme) => ({
     switchBase: {
         color: "rgba(49,53,56,255)",
         "&.Mui-checked": {
-            color: "rgba(49,53,56,255)"
+            color: "rgba(49,53,56,255)",
+            transform: "translateX(1.8rem)"
         },
         "&.Mui-checked + .MuiSwitch-track": {
             backgroundColor: "rgb(102, 252, 241)",
-            opacity: '1'
-        }
+            opacity: '1',
+        },
+    },
+    switchRoot: {
+        width: '4.6rem',
+        height: '2.6rem',
+    },
+    switchThumb: {
+        height: "1.5rem",
+        width: "1.5rem",
     },
     switchTrack: {
         backgroundColor: "rgba(224,224,224,255)",
-        opacity: "1"
+        opacity: "1",
+        height: "1.2rem",
+        width: "2.8rem",
+        borderRadius: "0.6rem",
     },
     sliderText: {
         color: "black",
-        fontSize: "0.8rem",
+        fontSize: "1.1rem",
         paddingLeft: "0",
-        alignItems: "bottom"
+        alignItems: "bottom",
+    },
+    midSwitchText: {
+        color: "black",
+        paddingLeft: "0",
+        fontSize: "1.2rem",
+        marginBottom: "4%",
     },
     switchText: {
         color: "black",
         paddingLeft: "0",
+        fontSize: "1.1rem",
     },
     gridItemRight: {
         textAlign: "right"
+    },
+    midGridItemRight: {
+        textAlign: "right",
+        marginBottom: "4%",
     },
     centerText: {
         textAlign: "center",
@@ -72,6 +113,7 @@ const useStyles = makeStyles((theme) => ({
     button: {
         backgroundColor: "rgba(49,53,56,255)",
         color: "white",
+        fontSize: "1.1rem",
         paddingLeft: "65px",
         paddingRight: "65px",
         boxShadow: 'none',
@@ -81,17 +123,19 @@ const useStyles = makeStyles((theme) => ({
     },
     inputPercent: {
         backgroundColor: "#606060",
-        color: "black",
+        color: "white",
         borderRadius: "50px",
-        width: "70px",
+        width: "75px",
         height: "35px",
+        paddingRight: "3%",
+        fontSize: "1rem",
     },
     unitText: {
-        color: "black"
+        color: "white",
     },
     buttonSection: {
         textAlign: "center",
-        paddingBottom: "25%"
+        paddingBottom: "4%"
     }
 }));
 
@@ -322,7 +366,7 @@ function StayAtHome(props) {
                 </Typography>
             </Grid>
             <Grid item xs={4} className={classes.gridItemRight}>
-                <Switch checked={state.Home} classes={{ track: classes.switchTrack, switchBase: classes.switchBase }} onChange={handleChange} name="stayAtHome" />
+                <Switch checked={state.Home} classes={{ track: classes.switchTrack, switchBase: classes.switchBase, thumb: classes.switchThumb, root: classes.switchRoot }} onChange={handleChange} name="stayAtHome" />
             </Grid>
         </Grid>
     );
@@ -345,36 +389,36 @@ function TargetedShutdown(props) {
         <FormGroup>
             <Grid container direction="row" justify="space-between" alignItems="center">
                 <Grid item xs={6}>
-                    <Typography className={classes.switchText}>
+                    <Typography className={classes.midSwitchText}>
                         Schools
                     </Typography>
                 </Grid>
-                <Grid item xs={6} className={classes.gridItemRight}>
-                    <Switch checked={state.Schools} classes={{ track: classes.switchTrack, switchBase: classes.switchBase }} onChange={handleChange} name="schoolsShutdown" />
+                <Grid item xs={6} className={classes.midGridItemRight}>
+                    <Switch checked={state.Schools} classes={{ track: classes.switchTrack, switchBase: classes.switchBase, thumb: classes.switchThumb, root: classes.switchRoot }} onChange={handleChange} name="schoolsShutdown" />
                 </Grid>
                 <Grid item xs={6}>
-                    <Typography className={classes.switchText}>
+                    <Typography className={classes.midSwitchText}>
                         Restaurants
                     </Typography>
                 </Grid>
-                <Grid item xs={6} className={classes.gridItemRight}>
-                    <Switch checked={state.Restaurants} classes={{ track: classes.switchTrack, switchBase: classes.switchBase }} onChange={handleChange} name="restaurantsShutdown" />
+                <Grid item xs={6} className={classes.midGridItemRight}>
+                    <Switch checked={state.Restaurants} classes={{ track: classes.switchTrack, switchBase: classes.switchBase, thumb: classes.switchThumb, root: classes.switchRoot }} onChange={handleChange} name="restaurantsShutdown" />
                 </Grid>
                 <Grid item xs={6}>
-                    <Typography className={classes.switchText}>
+                    <Typography className={classes.midSwitchText}>
                         Gyms
                     </Typography>
                 </Grid>
-                <Grid item xs={6} className={classes.gridItemRight}>
-                    <Switch checked={state.Gyms} classes={{ track: classes.switchTrack, switchBase: classes.switchBase }} onChange={handleChange} name="gymsShutdown" />
+                <Grid item xs={6} className={classes.midGridItemRight}>
+                    <Switch checked={state.Gyms} classes={{ track: classes.switchTrack, switchBase: classes.switchBase, thumb: classes.switchThumb, root: classes.switchRoot }} onChange={handleChange} name="gymsShutdown" />
                 </Grid>
                 <Grid item xs={6}>
-                    <Typography className={classes.switchText}>
+                    <Typography className={classes.midSwitchText}>
                         Bars
                     </Typography>
                 </Grid>
-                <Grid item xs={6} className={classes.gridItemRight}>
-                    <Switch checked={state.Bars} classes={{ track: classes.switchTrack, switchBase: classes.switchBase }} onChange={handleChange} name="barsShutdown" />
+                <Grid item xs={6} className={classes.midGridItemRight}>
+                    <Switch checked={state.Bars} classes={{ track: classes.switchTrack, switchBase: classes.switchBase, thumb: classes.switchThumb, root: classes.switchRoot }} onChange={handleChange} name="barsShutdown" />
                 </Grid>
             </Grid>
         </FormGroup>
@@ -496,11 +540,15 @@ function sanitizePercentInput(input) {
 export default function ConfigurationsPanel(props) {
     const classes = useStyles();
 
+    const mobile = useMediaQuery('(max-width:600px)');
+
+    console.log(mobile);
+
     let configs = JSON.parse(JSON.stringify(props.configs)); //Create a deep copy of parent configurations
 
     return (
-        <div className={classes.root}>
-            <div className={classes.panelSection}>
+        <div className={mobile ? classes.mobileRoot : classes.root}>
+            <div className={mobile ? classes.mobilePanelSection : classes.panelSection}>
                 <Typography className={classes.configurationsHeader}>
                     NPI's
                 </Typography>
@@ -509,13 +557,13 @@ export default function ConfigurationsPanel(props) {
                 <MassSlider configs={configs} />
                 <StayAtHome configs={configs} />
             </div>
-            <div className={classes.panelSection}>
+            <div className={mobile ? classes.mobilePanelSection : classes.panelSection}>
                 <Typography className={classes.configurationsHeader}>
                     Targeted Shutdowns
                 </Typography>
                 <TargetedShutdown configs={configs} />
             </div>
-            <div className={classes.confirmButtonPanel}>
+            <div className={mobile ? classes.mobilePanelSection : classes.confirmButtonPanel}>
                 <div>
                     <Typography className={classes.configurationsHeader}>
                         Vaccinations
