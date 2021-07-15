ec2cc0c26b23e97d7bf645f0a7ea314e033b1c49
GWCMelody <melody2011102@gmail.com>
2021-07-15 16:48:29 -0400

    moved graphs to okc simulation page

---
+++ model_app/frontend/src/components/Graphs/DailyChart.js
@@ -0,0 +1,104 @@
+import { useD3 } from './useD3';
+import React from 'react';
+import * as d3 from 'd3';
+
+function DailyChart(props) {
+    var data = props.data[0]
+    const height = props.height;
+    const width = props.width;
+
+    const ref = useD3(
+        (svg) => {
+            const margin = { top: 30, right: 250, bottom: 30, left: 80 };
+            var casedata = data.data;
+            svg.append("text").attr("x", margin.left + (width - margin.left - margin.right) / 2).attr("y", 20).style('fill', 'white').style("text-anchor", "middle").style("font-family", "Montserrat").text("Daily Count v. Time");
+
+            const x = d3
+                .scaleBand()
+                .domain(casedata.map((d) => d.date.month + "/" + d.date.day + "/" + d.date.year))
+                .rangeRound([margin.left, width - margin.right])
+                .padding(0.1);
+
+            const max = d3.max(casedata, (d) => d.newcases)
+
+            const y = d3
+                .scaleLinear()
+                .domain([0, 1.1 * max])
+                .rangeRound([height - margin.bottom, margin.top]);
+
+            const xAxis = (g) =>
+                g
+                    .attr("transform", `translate(0,${height - margin.bottom})`)
+                    .style("color", "#66FCF1")
+                    .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0))
+                    .call((g) =>
+                        g
+                        .append("text")
+                        .attr("class", "x label")
+                        .attr("fill", "currentColor")
+                        .attr("text-anchor", "middle")
+                        .attr("x", width / 3)
+                        .attr("y", margin.bottom + 20)
+                        .style("fill", "#66FCF1")
+                        .style("font-family", "Montserrat")
+                        .style("font-size", '1rem')
+                        .text("Date")
+                    );
+
+            const yAxis = (g) =>
+                g
+                    .attr("transform", `translate(${margin.left},0)`)
+                    .style("color", "#66FCF1")
+                    .call(d3.axisLeft(y).ticks(null, "s").tickSizeOuter(0))
+                    .call((g) =>
+                        g
+                            .append("text")
+                            .attr("x", -height / 2)
+                            .attr("y", -50)
+                            .attr("fill", "currentColor")
+                            .attr("text-anchor", "middle")
+                            .attr("transform", "rotate(-90)")
+                            .style("font-family", "Montserrat")
+                            .style("font-size", '1rem')
+                            .text("Daily Infection Count")
+                    );
+
+            svg.select(".x-axis").call(xAxis);
+            svg.select(".y-axis").call(yAxis);
+
+            svg
+                .append("path")
+                .datum(casedata)
+                .attr("fill", "none")
+                .attr("stroke", "#66FCF1")
+                .attr("stroke-width", 1.5)
+                .attr('preserveAspectRatio','none')
+                .attr("d", d3.line()
+                    .x(function (d) {
+                        return x(d.date.month + "/" + d.date.day + "/" + d.date.year)
+                    })
+                    .y(function (d) {
+                        return y(d.newcases)
+                    })
+                );
+        },
+        [data.length]
+    );
+
+    return (
+        <svg
+            ref={ref}
+            style={{
+                height: 500,
+                width: "100%",
+                marginRight: "0px",
+                marginLeft: "0px",
+            }}
+        >
+            <g className="x-axis" />
+            <g className="y-axis" />
+        </svg>
+    );
+}
+
+export default DailyChart;
--- model_app/frontend/src/containers/GeneralSimulator.js
+++ model_app/frontend/src/containers/GeneralSimulator.js
@@ -1,8 +1,162 @@
 import React, { Component } from "react";
 import "./GeneralSimulator.css";
 import ConfigurationsPanel from "../components/ConfigurationsPanel";
-import { Typography } from "@material-ui/core";
+import { Typography, Grid } from "@material-ui/core";
 import okc_city from "../images/OKC_CITY.jpg";
+import DailyChart from "../components/Graphs/DailyChart"
+import CumulativeChart from "../components/Graphs/CumulativeChart"
+import PieChart from "../components/Graphs/PieChart"
+import Graph4 from "../components/Graphs/Graph4"
+
+var getData = () => {
+  var data = [];
+
+  // axios
+  //     .get("https://covidmod.isi.jhu.edu", {
+  //         params: {
+  //             year: '2020'
+  //         }
+  //     })
+  //     .then((res) => {
+  //         data = res.data;
+  //         console.log("data retrieved");
+  //     })
+  //     .catch((err) => {
+  //         if (axios.isCancel(err)) {
+  //             console.log("Request canceled:", err.message);
+  //         } else {
+  //             console.log(err);
+  //         }
+  //     });
+  // return data;
+
+  //copied dummy data for now
+  data = [
+    {
+      case_distribution: [
+        {
+          label: 'school',
+          value: 23
+        },
+        {
+          label: 'restaurant',
+          value: 10
+        },
+        {
+          label: 'gym',
+          value: 38
+        },
+        {
+          label: 'bar',
+          value: 29
+        }
+      ],
+
+      initial_cases: 0,
+
+      data: [
+        {
+          'date': {
+            'year': '2020',
+            'month': '04',
+            'day': '02'
+          },
+          'newcases': '24'
+        },
+
+        {
+          'date': {
+            'year': '2020',
+            'month': '04',
+            'day': '03'
+          },
+          'newcases': '21'
+        },
+
+        {
+          'date': {
+            'year': '2020',
+            'month': '04',
+            'day': '04'
+          },
+          'newcases': '19'
+        },
+
+        {
+          'date': {
+            'year': '2020',
+            'month': '04',
+            'day': '05'
+          },
+          'newcases': '9'
+        },
+
+        {
+          'date': {
+            'year': '2020',
+            'month': '04',
+            'day': '06'
+          },
+          'newcases': '14'
+        },
+
+        {
+          'date': {
+            'year': '2020',
+            'month': '04',
+            'day': '07'
+          },
+          'newcases': '24'
+        },
+
+        {
+          'date': {
+            'year': '2020',
+            'month': '04',
+            'day': '08'
+          },
+          'newcases': '14'
+        },
+
+        {
+          'date': {
+            'year': '2020',
+            'month': '04',
+            'day': '09'
+          },
+          'newcases': '68'
+        },
+
+        {
+          'date': {
+            'year': '2020',
+            'month': '04',
+            'day': '10'
+          },
+          'newcases': '28'
+        }
+      ]
+    }
+
+  ]
+
+  getTotalCases(data[0])
+
+  return data;
+}
+
+function getTotalCases(data) {
+  var totalCount = data.initial_cases
+  Object.keys(data.case_distribution).forEach(function (key, i) {
+    console.log(data.case_distribution[key])
+  })
+  return data['data'].map((entry, i) => {
+    totalCount += parseInt(entry['newcases'])
+    entry['totalcases'] = totalCount
+    entry['newcases'] = parseInt(entry['newcases'])
+    return 0;
+  })
+}
 
 class GeneralSimulator extends Component {
   constructor() {
@@ -18,7 +172,8 @@ class GeneralSimulator extends Component {
         gymsShutdown: false,
         barsShutdown: false,
         vaccinePercent: 30,
-      }
+      },
+      data: getData()
     }
   }
 
@@ -31,66 +186,86 @@ class GeneralSimulator extends Component {
 
   render() {
     return (
-      
-      <div className="content">
-        <h1
-        style={{
-          color: "white",
-          marginTop: "2%",
-          marginBottom: "1%"
-        }}>
-        Oklahoma City
-        </h1>
-        <div>
-          <img
-            img src={okc_city} alt="okc"
-            height={100}
-            width={375}
-            style={{
-              alignSelf: 'center',
-              marginBottom: "1%",
-            }}
-          />
-        </div>
 
-        <div>
-          {/*PLEASE READ:
+      <div className="content">
+        <Grid container spacing={3}>
+          <Grid item xs={4}>
+            <CumulativeChart data={this.state.data} height={300} width={600}/>
+          </Grid>
+          <Grid item xs={4}>
+            <h1
+              style={{
+                color: "white",
+                marginTop: "2%",
+                marginBottom: "1%"
+              }}>
+              Oklahoma City
+            </h1>
+            <div>
+              <img
+                img src={okc_city} alt="okc"
+                height={100}
+                width={375}
+                style={{
+                  alignSelf: 'center',
+                  marginBottom: "1%",
+                }}
+              />
+            </div>
+          </Grid>
+          <Grid item xs={4}>
+            <DailyChart data={this.state.data} height={300} width={600} />
+          </Grid>
+          <Grid item xs={4}>
+            <PieChart data={this.state.data[0].case_distribution} innerRadius={50} outerRadius={150} />
+          </Grid>
+          <Grid item xs={4}>
+
+
+
+            <div>
+              {/*PLEASE READ:
           ======================================================================================
           All the stuff below in this div are not needed, someone on Haley/Ryan's scrum team
           was kind enough to leave this here to show how to access the various state variables.
           Please remove and replace with the Population and POI info (see figma) when ready.
            */}
-          <Typography>
-            mask wearing percent: {this.state.configurations.maskPercent}
-          </Typography>
-          <Typography>
-            capacity restrictions: {this.state.configurations.capacityPercent}
-          </Typography>
-          <Typography>
-            mass testing: {this.state.configurations.massPercent}
-          </Typography>
-          <Typography>
-            stay at home order: {this.state.configurations.stayAtHome.toString()}
-          </Typography>
-          <Typography>
-            schools: {this.state.configurations.schoolsShutdown.toString()}
-          </Typography>
+              <Typography>
+                mask wearing percent: {this.state.configurations.maskPercent}
+              </Typography>
+              <Typography>
+                capacity restrictions: {this.state.configurations.capacityPercent}
+              </Typography>
+              <Typography>
+                mass testing: {this.state.configurations.massPercent}
+              </Typography>
+              <Typography>
+                stay at home order: {this.state.configurations.stayAtHome.toString()}
+              </Typography>
+              <Typography>
+                schools: {this.state.configurations.schoolsShutdown.toString()}
+              </Typography>
 
-          <Typography>
-            restaurants: {this.state.configurations.restaurantsShutdown.toString()}
-          </Typography>
-          <Typography>
-            gyms: {this.state.configurations.gymsShutdown.toString()}
-          </Typography>
-          <Typography>
-            bars: {this.state.configurations.barsShutdown.toString()}
-          </Typography>
-          <Typography>
-            vaccination percent: {this.state.configurations.vaccinePercent}
-          </Typography>
+              <Typography>
+                restaurants: {this.state.configurations.restaurantsShutdown.toString()}
+              </Typography>
+              <Typography>
+                gyms: {this.state.configurations.gymsShutdown.toString()}
+              </Typography>
+              <Typography>
+                bars: {this.state.configurations.barsShutdown.toString()}
+              </Typography>
+              <Typography>
+                vaccination percent: {this.state.configurations.vaccinePercent}
+              </Typography>
 
-        </div>
-        <ConfigurationsPanel updateConfigs={this.updateConfigurations} configs={this.state.configurations}/>
+            </div>
+          </Grid>
+          <Grid item xs={4}>
+            <Graph4 data={this.state.data} height={300} width={600}></Graph4>
+          </Grid>
+        </Grid>
+        <ConfigurationsPanel updateConfigs={this.updateConfigurations} configs={this.state.configurations} />
       </div >
     );
   }
