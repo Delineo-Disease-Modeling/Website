39d334ccff7f56ac9aadb649ca73921a6420e689
SP-Zeng <77908144+SP-Zeng@users.noreply.github.com>
2021-09-17 23:23:05 -0400

    add some basic test for dev blog page

---
+++ model_app/frontend/src/containers/DevBlog.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Adapter from "enzyme-adapter-react-16";
+import { shallow, configure } from "enzyme";
+
+import DevBlog from "./DevelopmentBlog";
+import DevBlogData from '../const/devblogposts';
+import { render } from "@testing-library/react";
+
+configure({ adapter: new Adapter() });
+
+describe("Development Blog", () => {
+     // check to see if Dev Blog renders
+     it("renders correctly", () => {
+       shallow(<DevBlog />);
+     });
+
+     // check for default display
+     it("carousel only display first blogposts", () => {
+          const { getByText} = render(<DevBlog />);
+
+          for (const i of DevBlogData) {
+     
+               if (i.id === 1) {
+               const title = getByText(i.title);
+               const subtext = getByText(i.subtext);
+               const author = getByText(i.author);
+               const date = getByText(i.date);
+               expect(title).toBeInTheDocument();
+               expect(subtext).toBeInTheDocument();
+               expect(author).toBeInTheDocument();
+               expect(date).toBeInTheDocument();
+               } else {
+               expect(() => getByText(i.title)).toThrow();
+               expect(() => getByText(i.subtext)).toThrow();
+               expect(() => getByText(i.author)).toThrow();
+               expect(() => getByText(i.date)).toThrow();
+               }     
+          }
+     });
+}
+);
