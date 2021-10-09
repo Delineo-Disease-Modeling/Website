d6375461717e76c4dccc20a683de5686018343f9
Brian Rui <Brian.Rui01@gmail.com>
2021-10-09 16:53:05 -0400

    Refactor from class to function components and clean up formatting

--- model_app/frontend/src/components/InfoTeamGridList.js
+++ model_app/frontend/src/components/InfoTeamGridList.js
@@ -1,15 +1,14 @@
 import GridList from './SectionGridList';
 import tileData from '../const/teamTileData.js';
 
-function InfoTeamGridList(role = 'all') {
-    var InfoTeamTileData = [];
-    var i = 0;
-    for (i = 0; i < tileData.length; i++) {
-        if (tileData[i].role === "Information Support") {
-            InfoTeamTileData.push(tileData[i]);
-        }
+const InfoTeamGridList = (role = 'all') => {
+  const InfoTeamTileData = [];
+  for (let i = 0; i < tileData.length; i++) {
+    if (tileData[i].role === "Information Support") {
+      InfoTeamTileData.push(tileData[i]);
     }
-    return GridList(InfoTeamTileData);
-}
+  }
+  return GridList(InfoTeamTileData);
+};
 
-export default InfoTeamGridList; 
+export default InfoTeamGridList;
