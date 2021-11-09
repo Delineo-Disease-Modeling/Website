bd99b40296f6bacf687400d512adb2ddb9002287
GWCMelody <melody2011102@gmail.com>
2021-11-08 20:21:59 -0500

    fixed text overlapping in pie chart, navbar alignment, yellow colored line on the Daily Infection Breakdown

--- model_app/frontend/src/components/Graphs/PieChart.js
+++ model_app/frontend/src/components/Graphs/PieChart.js
@@ -9,80 +9,96 @@ function PieChart(props) {
     innerRadius,
   } = props;
 
-  const margin = {
-    top: 50, right: 50, bottom: 50, left: 50,
-  };
-
-  const width = 2 * outerRadius + margin.left + margin.right;
-  const height = 2 * outerRadius + margin.top + margin.bottom;
-  const textColor = "#222629"
-
-  const colorScale = d3     
-    .scaleSequential()      
-    .interpolator(d3.interpolateCool)      
-    .domain([0, data.length]);
-
   useEffect(() => {
     drawChart();
   }, [data]);
 
   function drawChart() {
-    // Remove the old svg
-    d3.select('#pie-container')
-      .select('svg')
-      .remove();
+    // set the dimensions and margins of the graph
+    var margin = 10
+    var width = 600;
+    var height = 300;
 
-    // Create new svg
-    const svg = d3
-      .select('#pie-container')
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height)
-      .append('g')
-      .attr('transform', `translate(${width / 2}, ${height / 2})`);
-    
-    svg.append("text")
-    .attr("x", 0)
-    .attr("y", 0 - height/2 +margin.top -20).style('fill', textColor).style("text-anchor", "middle")
-    .style("font-family", "Montserrat").text("Infection breakdown by facility");
+    const textColor = "#222629"
 
+    // append the svg object to the div called 'pie-container'
+    var svg = d3.select("#pie-container")
+      .append("text").attr("x", (width + 2 * margin) / 2).attr("y", 20).style('fill', textColor).style("text-anchor", "middle").style("font-family", "Montserrat").text("Breakdown by Faculty")
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      // .attr("style", "-webkit-filter: drop-shadow( 0px 3px 3px rgba(0,0,0,.3) ); filter: drop-shadow( 0px 3px 3px rgba(0,0,0,.25) )")
+      .append("g")
+      .attr("transform", "translate(" + width / 3 + "," + height / 2 + ")");
 
-    const arcGenerator = d3
-      .arc()
-      .innerRadius(innerRadius)
-      .outerRadius(outerRadius);
+    const color = d3
+      .scaleSequential()
+      .interpolator(d3.interpolateCool)
+      .domain([0, data.length]);
 
     const pieGenerator = d3
       .pie()
       .padAngle(0)
       .value((d) => d.value);
 
-    const arc = svg
-      .selectAll()
-      .data(pieGenerator(data))
-      .enter();
+    var data_ready = pieGenerator(data)
 
-    // Append arcs
-    arc
+    // The arc generator
+    var arcGenerator = d3.arc()
+      .innerRadius(innerRadius)         // This is the size of the donut hole
+      .outerRadius(outerRadius)
+
+    // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+    svg
+      .selectAll('allSlices')
+      .data(data_ready)
+      .enter()
       .append('path')
       .attr('d', arcGenerator)
-      .style('fill', (_, i) => colorScale(i))
-      .style('stroke', '#ffffff')
-      .style('stroke-width', 0);
+      .attr('fill', (_, i) => color(i))
+      .attr("stroke", "white")
+      .style("stroke-width", "2px")
+      .style("opacity", 0.7)
 
-    // Append text labels
-    arc
+    // Add the polylines between chart and labels:
+    svg
+      .selectAll('allPolylines')
+      .data(data_ready)
+      .enter()
+      .append('polyline')
+      .attr("stroke", "black")
+      .style("fill", "none")
+      .attr("stroke-width", 1)
+      .attr('points', function (d) {
+        var posA = arcGenerator.centroid(d) // line insertion in the slice
+        var posB = arcGenerator.centroid(d) // line break: we use the other arc generator that has been built only for that
+        var posC = arcGenerator.centroid(d); // Label position = almost the same as posB
+        var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
+        posC[0] = ((outerRadius + margin) * 0.95) * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+        return [posA, posB, posC]
+      })
+
+    // Add the polylines between chart and labels:
+    svg
+      .selectAll('allLabels')
+      .data(data_ready)
+      .enter()
       .append('text')
-      .attr('text-anchor', 'middle')
-      .style("font-family", "Montserrat")
-      .attr('alignment-baseline', 'middle')
       .text((d) => `${d.data.label}: ${d.data.value}%`)
-      .style('fill', '#fff')
-      .attr('transform', (d) => {
-        const [x, y] = arcGenerator.centroid(d);
-        return `translate(${x}, ${y})`;
+      .attr('transform', function (d) {
+        var pos = arcGenerator.centroid(d);
+        var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+        pos[0] = (outerRadius + margin) * 0.99 * (midangle < Math.PI ? 1 : -1);
+        return 'translate(' + pos + ')';
+      })
+      .attr("style", "font-size: 0.7em;")
+      .style("font-family", "Montserrat")
+      .style('text-anchor', function (d) {
+        var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+        return (midangle < Math.PI ? 'start' : 'end')
       });
-  }    
+
+  }
 
   return <div id="pie-container" />;
 }
