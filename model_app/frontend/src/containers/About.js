d485c3df8e3543fd97dbfaaa352f875997e13ad5
Rosa <ygao112@jhu.edu>
2022-02-28 21:54:30 -0500

    stored form inputs to data object, fixed form style

--- model_app/frontend/src/containers/About.js
+++ model_app/frontend/src/containers/About.js
@@ -12,6 +12,7 @@ import faqData from '../const/faqData';
 import bgImg from '../images/Delineo-About.gif';
 import InfoBlock from '../components/InfoBlock';
 import './About.css'
+import axios from "axios";
 
 const styles = {
 	cardBackground: {
@@ -154,6 +155,7 @@ class About extends React.Component {
 		super();
 		this.MapDescription = "The Johns Hopkins Coronavirus Resource Center (CRC) is a continuously updated source of COVID-19 data and expert guidance. We aggregate and analyze the best data available on COVID-19 - including cases, as well as testing, contact tracing and vaccine efforts - to help the public, policymakers and healthcare professionals worldwide respond to the pandemic.";
 		this.SimulationDescription = "Viruses, such as the one that causes COVID-19, spread quickly through large cities because of a complex web of interactions between people taking place in a densely populated area. But how viruses move from person to person in smaller, rural communities is less well understood, resulting in public health and economic decisions that are made on the basis of scant information and overgeneralized modeling. The Delineo project is developing a distributed programming environment to run the model over large numbers of computers to scale up the areas that can be accurately modeled.";
+		this.handleSubmit= this.handleSubmit.bind(this);
 	}
 
 	render() {
@@ -213,11 +215,12 @@ class About extends React.Component {
 					</Typography>
 					<Typography varient='h1' component="h2" style={{ color: '#222629' }}>
 
-						<form style={{ padding: '0 50px' }} className={classes.form} noValidate autoComplete="off">
-							<input className={classes.input} id="input-id" type="text" placeholder="Name" required name="Name" />
-							<input className={classes.input} id="input-id" type="text" placeholder="Email" required name="Email" />
-							<input className={classes.input} id="input-id" type="text" placeholder="Subject" required name="Subject" />
-							<input className={classes.input} id="input-id" type="text" placeholder="Comment" required name="Comment" />
+						<form onSubmit={this.handleSubmit} id='contact-form' method="POST" style={{ padding: '0 50px' }} className={classes.form} noValidate autoComplete="off">
+							<input className={classes.input} id="firstName" type="text" placeholder="First Name" required name="FirstName" />
+							<input className={classes.input} id="lastName" type="text" placeholder="Last Name" required name="LastName" />
+							<input className={classes.input} id="email" type="email" placeholder="Email" required name="Email" />
+							<input className={classes.input} id="subject" type="text" placeholder="Subject" required name="Subject" />
+							<textarea className={classes.input} id="comment" placeholder="Comment" required name="Comment" />
 
 							<Button endIcon={<SendIcon />} className={classes.button} type="submit" size="large" varient='contained'>
 								SEND MESSAGE
@@ -229,5 +232,37 @@ class About extends React.Component {
 			</div >
 		);
 	}
+
+
+	handleSubmit(event) {
+		event.preventDefault();
+		const { firstName, lastName, email, subject, comment } = event.target.elements;
+		let data = {
+			firstName: firstName.value,
+			lastName: lastName.value,
+			email: email.value,
+			subject: subject.value,
+			comment: comment.value
+		};
+
+
+
+		console.log(data);
+
+		// let response = await fetch("http://localhost:3000/contact", {
+		// 	method: "POST",
+		// 	headers: {
+		// 		"Content-Type": "application/json;charset=utf-8",
+		// 	},
+		// 	body: JSON.stringify(data),
+		// });
+
+		// let result = await response.json();
+
+
+	}
+
+	
+	
 }
 export default withStyles(styles)(About);
