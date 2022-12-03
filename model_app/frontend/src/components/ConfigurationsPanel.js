1f37ebda97e84622f44df3952f50d07a0de925f9
melodyhplee <lee.hsinpei.melody@gmail.com>
2022-12-02 21:42:14 -0500

    updated slider component

--- model_app/frontend/src/components/ConfigurationsPanel.js
+++ model_app/frontend/src/components/ConfigurationsPanel.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import FormControl from "@material-ui/core/FormControl";
 import Button from "@material-ui/core/Button";
 import Snackbar from "@material-ui/core/Snackbar";
-import TabPanel from "./TabPanel";
+import TabPanel from "./SimulationPanel";
 
 const useStyles = makeStyles((theme) => ({
   root: {
---
+++ model_app/frontend/src/components/InputSlider.js
@@ -0,0 +1,91 @@
+import * as React from 'react';
+import PropTypes from "prop-types";
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Slider from '@mui/material/Slider';
+import MuiInput from '@mui/material/Input';
+
+const Input = styled(MuiInput)`
+  width: 42px;
+`;
+
+const StyledSlider = styled(Slider)(({ theme }) => ({
+  color: "rgba(49,53,56,255)",
+  "& .MuiSlider-thumb": {
+    backgroundColor: "rgb(102, 252, 241)",
+    border: "2px solid currentColor",
+  }
+}));
+
+export default function InputSlider(props) {
+  const { label } = props;
+  const [value, setValue] = React.useState(props.percentConfig);
+
+  const handleSliderChange = (event, newValue) => {
+    setValue(newValue);
+    // Commented out write to percentConfig 
+    // props.percentConfig = value;
+  };
+
+  const handleInputChange = (event) => {
+    const newValue = event.target.value === '' ? '' : Number(event.target.value);
+    setValue(newValue);
+    // props.percentConfig = value;
+  };
+
+  const handleBlur = () => {
+    if (value < 0) {
+      setValue(0);
+      // props.percentConfig = 0;
+    } else if (value > 100) {
+      setValue(100);
+      // props.percentConfig = 100;
+    }
+  };
+
+  return (
+    <div>
+      <Grid container direction="row" alignItems="center">
+        <Grid container spacing={2} alignItems="center">
+          <Grid item xs={2}>
+            <Typography id="input-slider" style={{ textAlign: 'left', color: 'black' }} gutterBottom>
+              {label}
+            </Typography>
+          </Grid>
+
+          <Grid item xs>
+            <StyledSlider
+              value={typeof value === 'number' ? value : 0}
+              onChange={handleSliderChange}
+              aria-labelledby="input-slider"
+            />
+          </Grid>
+          <Grid item>
+            <Input
+              value={value}
+              size="small"
+              onChange={handleInputChange}
+              onBlur={handleBlur}
+              inputProps={{
+                step: 1,
+                min: 0,
+                max: 100,
+                type: 'number',
+                'aria-labelledby': 'input-slider',
+              }}
+            />
+          </Grid>
+          <Grid item>
+            %
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+InputSlider.propTypes = {
+  label: PropTypes.string.isRequired,
+  percentConfig: PropTypes.number
+}
