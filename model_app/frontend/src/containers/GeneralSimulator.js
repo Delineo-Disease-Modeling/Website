cc27351ddef0080909636b9246e0005ccacc7c57
Christian Bakhit <chrisbakhit@gmail.com>
2023-01-29 21:07:49 -0500

    basic fixes

--- model_app/frontend/src/containers/GeneralSimulator.js
+++ model_app/frontend/src/containers/GeneralSimulator.js
@@ -10,7 +10,6 @@ import Grid from "@material-ui/core/Grid";
 import { Cell, Legend, Pie, PieChart } from "recharts";
 import { BarChart, Bar, CartesianGrid, XAxis, YAxis } from "recharts";
 import testdata from "../data/testdata.json";
-import axios from "axios";
 
 const styles = (theme) => ({
   boldTitle: {
@@ -23,7 +22,7 @@ const styles = (theme) => ({
     alignItems: "center",
     whiteSpace: "pre-wrap",
   },
-})
+});
 
 const data = [
   {
@@ -36,19 +35,19 @@ const data = [
   },
 ];
 
-// Typography for Parameter Summary 
+// Typography for Parameter Summary
 function SummaryTypography(props) {
-
   return (
     <div>
-      <Typography sytle={{ color: 'white' }}>
-        {props.parameter}:
-      </Typography>
-      <Typography variant='h6' style={{ color: 'red', fontWeight: 900, textAlign: 'center' }}>
+      <Typography sytle={{ color: "white" }}>{props.parameter}:</Typography>
+      <Typography
+        variant="h6"
+        style={{ color: "red", fontWeight: 900, textAlign: "center" }}
+      >
         {props.value}
       </Typography>
     </div>
-  )
+  );
 }
 
 class GeneralSimulator extends Component {
@@ -72,148 +71,116 @@ class GeneralSimulator extends Component {
   }
 
   //Update configurations once user presses confirm
-  updateConfigurations = async (configs) => {
+  updateConfigurations = async (configs, useDB) => {
     this.setState({
       configurations: configs,
     });
     try {
-      const res = await axios.post("http://localhost:3002/simulator/okc", configs, {
-      })
+      console.log(configs);
+      console.log("useDB: " + useDB);
+      const res = await axios
+        .post("https://covidmod.isi.jhu.edu/simulation/", configs, useDB, {})
+        .then((response) => console.log(response.data));
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   };
-
-  // Update configurations based on API call
-  // TODO: Use this functions once API is implemented
-  fetchConfigurations = async () => {
-    const simulationAPI = "https://covidweb.isi.jhu.edu/api/v1/run_simulation";
-    // TODO: Add body params here if finalized API requires so in request
-    const body = {};
-    
-    await axios.post(simulationAPI, body)
-      .then((response) => this.updateConfigurations(response.data))
-      .catch((error) => console.log(error));
-  }
 
   render() {
     const { classes } = this.props;
 
     return (
       <div className="content" style={{ backgroundColor: "#1F2325" }}>
-        <Typography variant="h3" className={classes.boldTitle}>Oklahoma City</Typography>
-
-        {/* Divide screen into left, middle, right */}
-        <Grid
-          container
-          justifyContent="center"
-          alignItems={"stretch"}
-          direction="row"
-          spacing={0}
+        <Typography variant="h3" className={classes.boldTitle}>
+          <b>COVID-19 Simulator</b>
+        </Typography>
+        <h4
+          style={{
+            color: "white",
+          }}
         >
-          {/* Left panel of screen = Summary */}
-          <Grid item xs={2} style={{ border: '4px solid white' }}>
+          <b>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </b>
+          <div
+            style={{
+              paddingBottom: "20px",
+            }}
+          ></div>
+        </h4>
+        {/* CONFIGURATIONS PANEL */}
+        <ConfigurationsPanel
+          updateConfigs={this.updateConfigurations}
+          configs={this.state.configurations}
+        />
 
-            {/* SUMMARY of parameter settings */}
-            <Card style={{ textAlign: "left", margin: 0, width: "100%", padding: "5px" }}>
-              <Typography variant="h5" style={{ color: 'white' }}>
-                Settings:
-              </Typography>
-              <SummaryTypography
-                parameter='mask wearing percent'
-                value={this.state.configurations.maskPercent}
+        {/* Bar Chart */}
+        <div
+          style={{
+            marginBottom: "20px",
+            marginTop: "20px",
+          }}
+        >
+          <BarChart
+            width={1200}
+            height={250}
+            data={testdata}
+            style={{
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            <Legend verticalAlign="bottom" height={36} />
+            <Bar dataKey="TotalNotInfected" stackId="a" fill="#8884d8" />
+            <Bar dataKey="TotalInfections" stackId="a" fill="#82ca9d" />
+            <CartesianGrid stroke="#ccc" />
+            <XAxis dataKey="BuildingName" />
+            <YAxis dataKey="TotalPeople" />
+          </BarChart>
+        </div>
+        <PieChart
+          width={350}
+          height={300}
+          style={{
+            marginLeft: "auto",
+            marginRight: "auto",
+          }}
+        >
+          <Pie
+            data={data}
+            color="#000000"
+            dataKey="count"
+            nameKey="name"
+            cx="50%"
+            cy="50%"
+            outerRadius={120}
+            fill="#8884d8"
+          >
+            {data.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={this.COLORS[index % this.COLORS.length]}
               />
-              <SummaryTypography
-                parameter='capacity restrictions'
-                value={this.state.configurations.capacityPercent}
-              />
-              <SummaryTypography
-                parameter='mass testing'
-                value={this.state.configurations.massPercent}
-              />
-              <SummaryTypography
-                parameter='stay at home order'
-                value={this.state.configurations.stayAtHome.toString()}
-              />
-              <SummaryTypography
-                parameter='schools'
-                value={this.state.configurations.schoolsShutdown.toString()}
-              />
-              <SummaryTypography
-                parameter='restaurants'
-                value={this.state.configurations.restaurantsShutdown.toString()}
-              />
-              <SummaryTypography
-                parameter='gyms'
-                value={this.state.configurations.gymsShutdown.toString()}
-              />
-              <SummaryTypography
-                parameter='bars'
-                value={this.state.configurations.barsShutdown.toString()}
-              />
-              <SummaryTypography
-                parameter='vaccination percent'
-                value={this.state.configurations.vaccinePercent}
-              />
-            </Card>
-          </Grid>
-
-          {/* Middle of screen - top: panel, bottom: chart */}
-          <Grid item xs={7} style={{ border: '4px solid white', padding: '10px' }}>
-            {/* CONFIGURATIONS PANEL */}
-            <ConfigurationsPanel
-              // TODO: replace line 162 with updateConfigs={this.fetchConfigurations}
-              updateConfigs={this.updateConfigurations}
-              configs={this.state.configurations}
-            />
-
-            {/* Infections Chart */}
-            <InfectionsChart />
-          </Grid>
-
-          {/* Right panel - more chart(s) */}
-          <Grid item xs={3} style={{ backgroundColor: "#1F2325", border: '4px solid white' }} >
-
-            {/* PIE CHART */}
-            <PieChart width={350} height={300}>
-              <Pie
-                data={data}
-                color="#000000"
-                dataKey="count"
-                nameKey="name"
-                cx="50%"
-                cy="50%"
-                outerRadius={120}
-                fill="#8884d8"
-              >
-                {data.map((entry, index) => (
-                  <Cell
-                    key={`cell-${index}`}
-                    fill={this.COLORS[index % this.COLORS.length]}
-                  />
-                ))}
-              </Pie>
-              <ToolTip description={"Percentage of infected individuals"} />
-              <Legend />
-            </PieChart>
-          </Grid>
-
-          {/* Bottom panel - more chart(s) */}
-          <Grid item xs={12} style={{ backgroundColor: "#1F2325", border: '4px solid white', padding: '10px' }}>
-
-            {/* Bar Chart */}
-            <BarChart width={1200} height={250} data={testdata}>
-              <Legend verticalAlign="bottom" height={36} />
-              <Bar dataKey="TotalNotInfected" stackId="a" fill="#8884d8" />
-              <Bar dataKey="TotalInfections" stackId="a" fill="#82ca9d" />
-              <CartesianGrid stroke="#ccc" />
-              <XAxis dataKey="BuildingName" />
-              <YAxis dataKey="TotalPeople" />
-            </BarChart>
-          </Grid>
-        </Grid>
-      </div >
-
+            ))}
+          </Pie>
+          <ToolTip description={"Percentage of infected individuals"} />
+          <Legend />
+        </PieChart>
+        <InfectionsChart
+          style={{
+            marginLeft: "50%",
+            marginRight: "50%",
+            display: "flex",
+            justifyContent: "center",
+          }}
+        />
+      </div>
     );
   }
 }
